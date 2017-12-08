623b790e16c1b50b63569d7325cd649c5652d8ef
gradek64 <someemail@yahoo.com>
2017-12-08 00:18:53 +0000

    routes and front page content strench

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ var note = require('./make_note_route');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.route('/').get(function(req, res, next) {
+   res.render('index',{ title: 'Express'});
 });
 
 // make note route;
@@ -15,33 +15,31 @@ router.route('/make_note').get(note.make_note);
 router.route('/make_note_submit').post(note.make_note_submit);
 router.route('/delete_note').post(note.delete_note);
 
-module.exports = router;
-
 //git pages;
-router.route('/git').get(function(req, res, next) {
+router.route('/git_esential_training_summary').get(function(req, res, next) {
   res.render('git/git_esential_training_summary');
 });
 
 //css staff;
-router.route('/CSS_all_selectors').get(function(req, res, next) {
+router.route('/CSS_all_selectors_refference').get(function(req, res, next) {
   res.render('css_staff/CSS_all_selectors_refference');
 });
-router.route('/CSS_all_selectors').get(function(req, res, next) {
+router.route('/click_activated_css3_js_together').get(function(req, res, next) {
   res.render('css_staff/click_activated_css3_js_together');
 });
 
 //vanila javascript 
 router.route('/array_methods').get(function(req, res, next) {
-  res.render('vanilla_js_routes/array_methods');
+  res.render('vanilla_js/array_methods');
 });
-router.route('/array_checks_and_loop').get(function(req, res, next) {
-  res.render('vanilla_js_routes/array_checks_and_loops');
+router.route('/array_checks_and_loops').get(function(req, res, next) {
+  res.render('vanilla_js/array_checks_and_loops');
 });
 router.route('/Object_properties_and_methods').get(function(req, res, next) {
-  res.render('vanilla_js_routes/Object_properties_and_methods');
+  res.render('vanilla_js/Object_properties_and_methods');
 });
 router.route('/String_methods').get(function(req, res, next) {
-  res.render('vanilla_js_routes/String_methods');
+  res.render('vanilla_js/String_methods');
 });
 
 //linux terminal commands 
